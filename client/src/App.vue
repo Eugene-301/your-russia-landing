9d82ae6dd30bf3c650f81b6288d4e9bd0cb09a44
Eugene-301 <ablepiha@duck.com>
2025-05-23 23:43:23 +0300

    Added fonts

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+  <h1>
+    Тралалело Тралалелоп
+  </h1>
 
 </template>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+  h1 {
+    color: black;
+    font-size: 50px;
+  }
 </style>
